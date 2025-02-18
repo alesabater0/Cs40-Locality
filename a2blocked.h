05d4baec25c1467f8dbc2074edc2a7df791aeb62
Alejandra Sabater <166755186+alesabater0@users.noreply.github.com>
2025-02-17 19:13:58 -0500

    Add files via upload

---
+++ a2blocked.h
@@ -0,0 +1,12 @@
+
+
+
+
+static A2 new(int width, int height, int size);
+static A2 new_with_blocksize(int width, int height, int size, int blocksize);
+static void a2free(A2 * array2p);
+static int width(A2 array2);
+static int height(A2 array2);
+
+
+
